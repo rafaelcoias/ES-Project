4a66beafb7377ba26ba2f68abc4dd09668c10304
André <andre.caraus@hotmail.com>
2024-04-23 23:54:47 +0100

    Revert "documentação editexcel"
    This reverts commit 8c1fb9854f9436abc72ec3799b80f768ac8e41d7.

--- src/pages/EditExcel.tsx
+++ src/pages/EditExcel.tsx
@@ -1,17 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import * as XLSX from "xlsx";
 
-/**
- * Componente para exibir e editar os dados de uma planilha do Excel.
- *
- * Este componente é responsável por exibir os dados de uma planilha do Excel em uma tabela
- * e permitir a edição desses dados.
- *
- * @returns {JSX.Element} O componente EditExcel.
- */
 export default function EditExcel() {
   const location = useLocation();
   const { tableData } = location.state;
+
 
   return (
     <div>
@@ -26,9 +20,9 @@ export default function EditExcel() {
           </tr>
         </thead>
         <tbody>
-          {tableData.map((row: any, rowIndex: number) => (
+          {tableData.map((row: number, rowIndex: number) => (
             <tr key={rowIndex}>
-              {Object.values(row).map((cell: any, cellIndex: number) => (
+              {Object.values(row).map((cell, cellIndex) => (
                 <td key={cellIndex}>{cell}</td>
               ))}
             </tr>
