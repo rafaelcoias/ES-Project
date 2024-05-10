61f050b5669f46fe2df1176f3bba4d56de7240a4
Guilherme Marques <guilhermeL.M-2002@outlook.com>
2024-05-10 19:11:28 +0100

    Styling after selections of heaat map and netgraphs Documentacao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -113,26 +113,32 @@ export default function Home() {
       <h1 className="text-[1.5rem] quatro:text-[2rem] font-bold">
         Bem-vindo de volta!
       </h1>
-      <button
-        onClick={() => navigate("/MarcarAula")}
-        className=" text-lg px-2 py-3 bg-[var(--blue)] text-white rounded-[13px] hover:bg-[var(--white)] hover:text-[var(--blue)] hover:border-[var(--blue)] border border-transparent transition-all duration-300">
-        Marcar Aula de Substituição
-      </button>
-      <button
-        onClick={() => navigate("/HeatMapGenerator")}
-        className=" text-lg px-2 py-3 bg-[var(--blue)] text-white rounded-[13px] hover:bg-[var(--white)] hover:text-[var(--blue)] hover:border-[var(--blue)] border border-transparent transition-all duration-300">
-        HeatMap
-      </button>
-      <button
-        onClick={() => navigate("/NetworkGraph")}
-        className=" text-lg px-2 py-3 bg-[var(--blue)] text-white rounded-[13px] hover:bg-[var(--white)] hover:text-[var(--blue)] hover:border-[var(--blue)] border border-transparent transition-all duration-300">
-        NetworkGraph
-      </button>
-      <button
-        onClick={() => navigate("/NetworkGraph")}
-        className=" text-lg px-2 py-3 bg-[var(--blue)] text-white rounded-[13px] hover:bg-[var(--white)] hover:text-[var(--blue)] hover:border-[var(--blue)] border border-transparent transition-all duration-300">
-        Marcar nova UC
-      </button>
+      <div className="flex w-full gap-6">
+        <div className="w-1/2 flex flex-col gap-6">
+          <button
+            onClick={() => navigate("/MarcarAula")}
+            className="text-lg px-2 py-3 bg-[var(--blue)] text-white rounded-[13px] hover:bg-[var(--white)] hover:text-[var(--blue)] hover:border-[var(--blue)] border border-transparent transition-all duration-300">
+            Marcar Aula de Substituição
+          </button>
+          <button
+            onClick={() => navigate("/NetworkGraph")}
+            className="text-lg px-2 py-3 bg-[var(--blue)] text-white rounded-[13px] hover:bg-[var(--white)] hover:text-[var(--blue)] hover:border-[var(--blue)] border border-transparent transition-all duration-300">
+            Marcar nova UC
+          </button>
+        </div>
+        <div className="w-1/2 flex flex-col gap-6">
+          <button
+            onClick={() => navigate("/HeatMapGenerator")}
+            className="text-lg px-2 py-3 bg-[var(--blue)] text-white rounded-[13px] hover:bg-[var(--white)] hover:text-[var(--blue)] hover:border-[var(--blue)] border border-transparent transition-all duration-300">
+            HeatMap
+          </button>
+          <button
+            onClick={() => navigate("/NetworkGraph")}
+            className="text-lg px-2 py-3 bg-[var(--blue)] text-white rounded-[13px] hover:bg-[var(--white)] hover:text-[var(--blue)] hover:border-[var(--blue)] border border-transparent transition-all duration-300">
+            NetworkGraph
+          </button>
+        </div>
+      </div>
       <hr className="ml-16 w-[90%] border-gray-400 my-4" />
       <p className="text-black text-xl">Ficheiros:</p>
       <div className="grid grid-cols-1 gap-4 oito:grid-cols-2">
@@ -175,6 +181,6 @@ export default function Home() {
           <input type="file" onChange={handleFileChange} />
         </div>
       </div>
-    </div>
+    </div >
   );
 }
